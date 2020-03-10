Bugfix in reward sum calculation

trickster/callbacks/evaluation.py
@@ -19,7 +19,7 @@ class TrajectoryEvaluator(Callback):
         for r in range(self.repeats):
             test_history = self.testing_rollout.rollout(verbose=0, push_experience=False)
             rewards.append(test_history["reward_sum"])
-        history.append(**{"RWD/sum": np.sum(rewards), "RWD/std": np.std(rewards)})
+        history.append(**{"RWD/sum": np.mean(rewards), "RWD/std": np.std(rewards)})
 
 
 class TrajectoryRenderer(Callback):

trickster/rollout/trajectory.py
@@ -113,7 +113,7 @@ class Trajectory(RolloutBase):
                 rewards.append(rollout_history["reward_sum"])
                 callbacks.on_batch_end()
 
-            train_history.append(**{"RWD/sum": np.sum(rewards), "RWD/std": np.std(rewards)})
+            train_history.append(**{"RWD/sum": np.mean(rewards), "RWD/std": np.std(rewards)})
 
             agent_history = self.agent.fit(batch_size=update_batch_size)
 